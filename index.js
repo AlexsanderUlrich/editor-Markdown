8f479db8fead0e323d0b2d42f0d2075ceea3a2f8
Alexsander Ulrich <alexsander@rsdata.inf.br>
2025-04-17 15:51:42 -0300

    Ajustes

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function enterPiP() {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Editor PiP</title>
-      <link rel="stylesheet" href="node_modules/simplemde/dist/simplemde.min.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplemde@1.11.2/dist/simplemde.min.css">
       <link rel="stylesheet" href="index.css" />
       <style>
         html, body {
